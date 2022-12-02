1680b9b70219e25fbd48e312cd041d0469b2714b
Tomas Guzman <tomasg92@gmail.com>
2022-12-01 22:35:56 -0400

    Updates sass

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,29 +1,3 @@
-// const jwt = require("jsonwebtoken")
-
-// exports.auth = (req, res, next) => {
-//   // Get token from header
-//   let token = req.header("Authorization")
-
-//   // Check if not token
-//   if (!token) {
-//     return res.status(401).json({
-//       msg: "No token, authorization denied",
-//     })
-//   }
-
-//   token = token.replace("Bearer ", "")
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-//     req.userId = decoded._id
-//     console.log("req.userId :>> ", req.userId)
-//   } catch (error) {
-//     return res.status(401).json({ msg: "Token is not valid" })
-//   }
-
-//   next()
-// }
-
 const jwt = require('jsonwebtoken')
 
 exports.auth = (req, res, next) => {
