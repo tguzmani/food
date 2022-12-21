44ad5eda7cf4d0800626ef8ff961fd7111a7a52a
Tomás <tomasg92@gmail.com>
2022-12-21 19:21:02 -0400

    Corrects food.routes file

--- server/foods/foods.routes.js
+++ server/foods/foods.routes.js
@@ -2,26 +2,15 @@ const express = require('express')
 const router = express.Router()
 const { auth } = require('../middleware/auth')
 
-const {
-  createFoodValidator,
-  updateFoodValidator
-} = require('../foods/foods.validators')
-
 const foodsController = require('../foods/foods.controller')
 
-router.post(
-  '/',
-  [auth, ...createFoodValidator],
-  foodsController.createFood
-)
+router.post('/', auth, foodsController.createFood)
 
 router.get('/', auth, foodsController.readFoodsByUserId)
 
-router.put(
-  '/:foodId',
-  [auth, ...updateFoodValidator],
-  foodsController.updateFood
-)
+router.put('/:foodId', auth, foodsController.updateFood)
+
+router.delete('/all', auth, foodsController.deleteAllFoodsFromDay)
 
 router.delete('/:foodId', auth, foodsController.deleteFood)
 
